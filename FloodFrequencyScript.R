0c23eab1e2e5dc1fa27c9bc83c032e0073d888e8
Lejo Flores <lejoflores@boisestate.edu>
2023-03-11 16:39:59 -0700

    update gumbel calc

--- FloodFrequencyScript.R
+++ FloodFrequencyScript.R
@@ -33,15 +33,18 @@ s_x <- sd(q_sort)
 alpha <- sqrt(6)*s_x / pi
 u <- xbar - 0.5772*alpha
 
-p_e_gumbel <- 1 - exp(-exp(-(q_sort - u)/alpha))
-t_gumbel <- 1 / p_e_gumbel 
+p_e_gumbel <- seq(from = 0.001, to = 0.999, by = 0.001)
+t_gumbel <- 1 / (1 - p_e_gumbel)
+
+q_gumbel = u - alpha*log(-log(p_e_gumbel))
 
 p2 <- ggplot() + 
   geom_point(data = NULL, aes(t_gringorten,q_sort), color='red') + 
   geom_point(data = NULL, aes(t_weibull,q_sort), color='blue') + 
-  geom_line(data = NULL, aes(t_gumbel,q_sort), color='chartreuse4') +
+  geom_line(data = NULL, aes(t_gumbel,q_gumbel), color='chartreuse4') +
   scale_x_continuous(trans = 'log10') + 
   scale_y_continuous(labels = comma) +
   labs(title = paste('Flood Frequency for Site',site_id, station_nm), 
   x = 'Return Interval [Years]', y = 'Peak Flow [cfs]')
 
+p2
